801db7cfc595d8c8ca7327734f37959885697ff9
bobby-dazzler <philipanderson@outlook.com>
2019-10-15 07:11:42 +0100

    added float and int runtime set

---
+++ Base/IntRuntimeSet.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEventsFramework;
+
+[CreateAssetMenu(menuName="Unity Events Framework/Runtime Sets/Int")]
+public class IntRuntimeSet : RuntimeSet<int> {
+    
+}
