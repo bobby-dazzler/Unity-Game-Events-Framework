87105180cca84451e0ed17c04b643d7265f9267a
Anderson <andersonp@hph.local>
2019-12-30 10:30:33 +0000

    Vector4 runtime set added

---
+++ Base/Vector4RuntimeSet.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEventsFramework;
+
+[CreateAssetMenu(menuName="Unity Events Framework/Runtime Sets/Vector4")]
+public class Vector4RuntimeSet : RuntimeSet<Vector4> {
+    
+}
