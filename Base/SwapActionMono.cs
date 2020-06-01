f13c56dfd1b51ebeaf7f3230ea45d9b20d411c3c
bobby-dazzler <philipanderson@outlook.com>
2020-06-01 10:54:29 +0100

    0.0.4

---
+++ Base/SwapActionMono.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityEventsFramework {
+    public class SwapActionMono : MonoBehaviour {
+        
+        public int actionIndex;
+
+        public Action[] actions;
+
+        public void SwapActionAtIndex(int index) {
+            StateController controller = GetComponent<StateController>();
+            State state = controller.currentState;
+            state.actions[actionIndex] = actions[index];
+        }
+    }
+}
